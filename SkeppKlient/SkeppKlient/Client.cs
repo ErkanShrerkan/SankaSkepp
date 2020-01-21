82b822951108b4e55c1214896389d7f4da9d27b7
ErkanShrerkan <43791546+ErkanShrerkan@users.noreply.github.com>
2020-01-21 22:40:52 +0100

    jaja

---
+++ SkeppKlient/SkeppKlient/Client.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SkeppKlient
+{
+    class Client
+    {
+        // allt är private för att de inte används utanför denna klass
+        int port = 8080;
+        private const int bufferSize = 1024; // max data per meddelande i bytes // const för annars squiggly
+        private static readonly byte[] buffer = new byte[bufferSize];
+        static bool isConnected = false; // blir true när man anslutit
+        static readonly Socket serverSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // deklareras här för tillgång senare, måste vara static
+
+        public Client()
+        {
+            CheckConnections(CleanArp(GetArp()));
+        }
+
+        private static List<string> CleanArp(string arp)
+        {
+            List<string> listOfIPs = new List<string>();
+            string ipRange = @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b";
+
+            // Create a Regex  
+            Regex regex = new Regex(ipRange);
+            // Get all matches  
+            MatchCollection matchedIPs = regex.Matches(arp);
+            // Print all matched IPs
+            for (int i = 0; i < matchedIPs.Count; i++)
+            {
+                listOfIPs.Add(matchedIPs[i].Value);
+            }
+
+            Console.WriteLine(listOfIPs[0]);
+
+            return listOfIPs;
+        }
+
+        private static string GetArp()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.StandardInput.WriteLine("arp -a");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            //Console.WriteLine(process.StandardOutput.ReadToEnd());
+            return process.StandardOutput.ReadToEnd().ToString();
+        }
+
+        private void CheckConnections(List<string> ipList)
+        {
+            Console.WriteLine("Please wait while a server is found, this can take a moment");
+            if (!serverSocket.Connected) // man kan inte ansluta
+            {
+                for (int i = 0; i < ipList.Count; i++)
+                {
+                    try
+                    {
+                        Console.WriteLine("Trying to connect to server {0}/{1}...", (i + 1), ipList.Count);
+                        serverSocket.Connect(ipList[i], port); // ansluter till serverns socket
+
+                        serverSocket.BeginReceive(buffer, 0, bufferSize, SocketFlags.None, ListenForData, serverSocket);
+                        Thread.Sleep(100);
+
+                        if (isConnected)
+                        {
+                            break;
+                        }
+                    }
+                    catch (SocketException ex)
+                    {
+                        // något gick fel
+                    }
+                }
+            }
+
+            if (!serverSocket.Connected)
+            {
+                Console.WriteLine("No game servers found");
+            }
+        }
+
+        private static void ListenForData(IAsyncResult AR) // körs i bakgrunden 
+        {
+            Socket current = (Socket)AR.AsyncState; // samma klient som innan
+            int received; // mängden bytes som tas emot
+
+            try
+            {
+                received = current.EndReceive(AR); // data från servern
+            }
+            catch (SocketException) // ingen endreceive betyder ingen klient
+            {
+                Console.WriteLine("Lost connection to server");
+                current.Close(); // stänger ner kopplingen mellan server och klient så att klienten kan koppla upp igen efter omstart
+                //clients.Remove(current); // tar bort den nuvarande socketen om den in
+                return; // om socketexception skippas resten av metoden
+            }
+
+            byte[] receiveBuffer = new byte[received]; // bytearray som lika lång som den in
+            Array.Copy(buffer, receiveBuffer, received); // kopierar buffer till receiveBuffer index 0 till received
+            string message = Encoding.UTF8.GetString(receiveBuffer); // gör konverterar receivebuffer från bytes till UTF8 och lägger i en string
+
+            if (message == "Welcome to battleship!")
+            {
+                isConnected = true;
+                Console.WriteLine("Connected to server");
+            }
+
+            //PrepareResponse();
+
+            current.BeginReceive(buffer, 0, bufferSize, SocketFlags.None, ListenForData, current); // kör metoden igen
+        }
+
+        private static void ReadResponse(IAsyncResult AR)
+        {
+            while (serverSocket.Connected) // så länge klienten är uppkopplad till servern
+            {
+                try
+                {
+                    var buffer = new byte[1024]; // max bytes per svar
+                    int received = serverSocket.Receive(buffer, SocketFlags.None); // antalet bytes i svaret
+                    if (received == 0) return; // svaret var 0 långt
+                    var data = new byte[received];
+                    Array.Copy(buffer, data, received);
+                    string response = Encoding.UTF8.GetString(data); // gör bytes till text
+                }
+                catch (Exception ex) // servern har stängts ner
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                    Environment.Exit(0); // crashar inte, jag stänger ner den för att inte krasha ok?
+                }
+            }
+        }
+
+        private static void CloseAllSockets(Socket socket) // stänger ner uppkopplingen till alla klienter
+        {
+            // stänger ner alla klienters sockets
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
---
+++ SänkaSkepp/SänkaSkepp/Server.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net; // behövs för att kommunicera över nätverk
+using System.Net.Sockets; // denna med
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SänkaSkepp
+{
+    class Server
+    {
+        // readonly för att inte kunna modifieras senare
+        readonly static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // deklareras för användning i alla klassens metoder om det behövs
+        static List<Socket> clients = new List<Socket>(); // list som alla unika ip addresser och portar förvaras. det är en list för att den kommer öka med tiden
+        private const int bufferSize = 2048; // max data per meddelande i bytes // const för annars squiggly
+        private readonly int port = 8080; // porten som servern lyssnar på
+        private static readonly byte[] buffer = new byte[bufferSize];
+
+        public Server()
+        {
+            StartServer(); // startar servern
+            Console.WriteLine("Press [Enter] to close the server");
+            Console.ReadLine(); // klickar man enter i servern stängs den ner
+            CloseAllSockets(); // stänger ner alla sockets innan server stänger ner
+        }
+
+        private void StartServer()
+        {
+            Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, port)); // serverns socket binder till den lokala addressen
+            serverSocket.Listen(12); // antalet anslutnings requests det får finnas utan en förlägning, inte min Listen
+            serverSocket.BeginAccept(AcceptClient, null); // async metod, körs tills servern stänger ner
+            Console.WriteLine("Server " + GetLocalIPAddress() + " is online... \n");
+        }
+
+        private static void AcceptClient(IAsyncResult AR) // kör i bakgrunden och ansluter till klienten
+        {
+            Socket socket; // klientsocket
+
+            try
+            {
+                // metoden hänger upp sig här i väntan på en client
+                socket = serverSocket.EndAccept(AR); // socketen godkänner uppkopplingen
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clients.Add(socket); // lägger clienten i en lista för användning senare
+            socket.BeginReceive(buffer, 0, bufferSize, SocketFlags.None, ListenForData, socket); // när anslutningen är godkänd börjar servern att lyssna
+            Console.WriteLine("Client connected");
+
+            byte[] data = Encoding.UTF8.GetBytes("Welcome to battleship!");
+            Send(data);
+
+            serverSocket.BeginAccept(AcceptClient, null); // kör denna metod igen 
+            // kan bara vänta på en client i taget
+        }
+
+        private static void ListenForData(IAsyncResult AR) // körs i bakgrunden 
+        {
+            Socket current = (Socket)AR.AsyncState; // samma klient som innan
+            int received; // mängden bytes som tas emot
+
+            try
+            {
+                received = current.EndReceive(AR); // data från clienten
+            }
+            catch (SocketException) // ingen endreceive betyder ingen klient
+            {
+                Console.WriteLine("Client disconnected");
+                current.Close(); // stänger ner kopplingen mellan server och klient så att klienten kan koppla upp igen efter omstart
+                clients.Remove(current); // tar bort den nuvarande socketen om den in
+                return; // om socketexception skippas resten av metoden
+            }
+
+            byte[] receiveBuffer = new byte[received]; // bytearray som lika lång som den in
+            Array.Copy(buffer, receiveBuffer, received); // kopierar buffer till receiveBuffer index 0 till received
+            string message = Encoding.UTF8.GetString(receiveBuffer); // gör konverterar receivebuffer från bytes till UTF8 och lägger i en string
+            Console.WriteLine("Received Text: " + message); // rå data från klienten innan manipulering
+
+            current.BeginReceive(buffer, 0, bufferSize, SocketFlags.None, ListenForData, current); // kör metoden igen
+        }
+
+        private static void Send(byte[] data)
+        {
+            try
+            {
+                for (int i = 0; i < clients.Count; i++) // för varje klient
+                {
+                    clients[i].Send(data); // skicka datan till klient nummer i i klientlistan tills alla klienter har fått ett svar
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString() + " Sending failed");
+            }
+            Console.WriteLine("Data sent to clients");
+        }
+
+        private static void CloseAllSockets() // stänger ner uppkopplingen till alla klienter
+        {
+            foreach (Socket socket in clients)
+            {
+                // stänger ner alla klienters sockets
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            // stänger till sist ner serverns socket
+            serverSocket.Close();
+        }
+
+        private string GetLocalIPAddress() // returnerar den lokala ipaddressen för servern
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList) // går igenom alla ipaddresser i det lokala nätverket
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!"); // om inte ipadressen hittades
+        }
+
+        /*private void BroadCast()
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, UDPPort));
+
+            var from = new IPEndPoint(0, 0);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var recvBuffer = udpClient.Receive(ref from);
+                    Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+                }
+            });
+
+            var data = Encoding.UTF8.GetBytes("ABCD");
+            udpClient.Send(data, data.Length, "255.255.255.255", UDPPort);
+        }*/
+    }
+}
